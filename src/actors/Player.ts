facaa944fb9ef913608c8d03ed61e914f6b41b7a
Mystichunterz <131076575+Mystichunterz@users.noreply.github.com>
2026-05-09 10:48:42 +0800

    Add WASD movement alongside arrow keys

--- src/actors/Player.ts
+++ src/actors/Player.ts
@@ -11,9 +11,17 @@ export interface PlayerBounds {
   y1: number;
 }
 
+interface WasdKeys {
+  W: Phaser.Input.Keyboard.Key;
+  A: Phaser.Input.Keyboard.Key;
+  S: Phaser.Input.Keyboard.Key;
+  D: Phaser.Input.Keyboard.Key;
+}
+
 export class Player {
   sprite: Phaser.GameObjects.Rectangle;
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+  private wasd: WasdKeys;
   private bounds: PlayerBounds;
 
   constructor(scene: Phaser.Scene, x: number, y: number, bounds: PlayerBounds) {
@@ -27,6 +35,7 @@ export class Player {
       throw new Error('Player: keyboard input not available on this scene');
     }
     this.cursors = scene.input.keyboard.createCursorKeys();
+    this.wasd = scene.input.keyboard.addKeys('W,A,S,D') as WasdKeys;
     scene.input.keyboard.addCapture('UP,DOWN,LEFT,RIGHT,W,A,S,D');
   }
 
@@ -34,10 +43,10 @@ export class Player {
     const dt = deltaMs / 1000;
     let dx = 0;
     let dy = 0;
-    if (this.cursors.left.isDown)  dx -= 1;
-    if (this.cursors.right.isDown) dx += 1;
-    if (this.cursors.up.isDown)    dy -= 1;
-    if (this.cursors.down.isDown)  dy += 1;
+    if (this.cursors.left.isDown  || this.wasd.A.isDown) dx -= 1;
+    if (this.cursors.right.isDown || this.wasd.D.isDown) dx += 1;
+    if (this.cursors.up.isDown    || this.wasd.W.isDown) dy -= 1;
+    if (this.cursors.down.isDown  || this.wasd.S.isDown) dy += 1;
     if (dx !== 0 || dy !== 0) {
       const len = Math.hypot(dx, dy);
       this.sprite.x += (dx / len) * SPEED * dt;
